f4253e2cf82439fb0b866d2a63e6624b2a34f13d
meera-k <45214387+meera-k@users.noreply.github.com>
2021-04-17 18:57:43 -0400

    added team info

---
+++ script.js
@@ -0,0 +1,4 @@
+<script type="text/javascript">
+var bannersnack_embed = {"hash":"b1c3g8i31","width":234,"height":60,"t":1600640537,"userId":41593803,"type":"html5"};
+</script>
+<script type="text/javascript" src="//cdn.bannersnack.com/iframe/embed.js"></script>
